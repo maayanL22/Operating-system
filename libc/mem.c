c7e5998b5643c09c16468015e467ed4e3955898a
maayanL22 <79762587+maayanL22@users.noreply.github.com>
2021-03-07 17:32:59 +0200

    Update mem.c

--- libc/mem.c
+++ libc/mem.c
@@ -13,7 +13,7 @@ void memory_set(uint8_t *dest, uint8_t val, uint32_t len) {
 }
 
 /* This should be computed at link time, but a hardcoded
- * value is fine for now. Remember that our kernel starts
+ * value is fine for now. The kernel starts
  * at 0x1000 as defined on the Makefile */
 uint32_t free_mem_addr = 0x10000;
 /* Implementation is just a pointer to some free memory which
